62d557e3e62a5527b98b60fe48a281ef652e085a
LiuYuJSCPPY <uioplret57423@gmail.com>
2022-06-06 15:25:21 +0800

    新增ProudctConteller

---
+++ EcommerceBookStore/EcommerceBookStore.Server/ProudctsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using EcommerceBookStore.Model;
+using EcommerceBookStore.Data;
+namespace EcommerceBookStore.Server
+{
+    public class ProudctsService
+    {
+        public IEnumerable<Proudct> GetAllProudcts()
+        {
+            var dbContext = new EBookStoreContext();
+
+            return dbContext.proudcts.Include(model => model.Category).Include(model => model.discount).ToList();
+        }
+    }
+}
