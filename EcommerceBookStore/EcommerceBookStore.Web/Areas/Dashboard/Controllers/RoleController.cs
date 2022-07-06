f7ef978e1ba0b4ca9fad1ba8a67248a77fa45498
LiuYuJSCPPY <uioplret57423@gmail.com>
2022-07-06 21:29:57 +0800

    修改

--- EcommerceBookStore/EcommerceBookStore.Web/Areas/Dashboard/Controllers/RoleController.cs
+++ EcommerceBookStore/EcommerceBookStore.Web/Areas/Dashboard/Controllers/RoleController.cs
@@ -3,11 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+using EcommerceBookStore.Model;
+using EcommerceBookStore.Data;
+using EcommerceBookStore.Server;
+using EcommerceBookStore.Web.Areas.Dashboard.ViewModel;
 
 namespace EcommerceBookStore.Web.Areas.Dashboard.Controllers
 {
     public class RoleController : Controller
     {
+        private EBookStoreSignInManager _signInManager;
+        private EBookStoreUserManager _userManager;
+
+        public RoleController()
+        {
+        }
+
+        public RoleController(EBookStoreUserManager userManager, EBookStoreSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public EBookStoreSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<EBookStoreSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public EBookStoreUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<EBookStoreUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+
+        }
+
+
+
         // GET: Dashboard/Role
         public ActionResult Index()
         {
